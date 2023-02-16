80bfc1871703d121a30af45e44e7ae9ebb2567d2
shyam3225 <shyamkurapati3225@gmail.com>
2023-02-16 10:58:12 +0530

    java message

---
+++ their.java
@@ -0,0 +1,7 @@
+public class className {
+    public static void main(String[] args) {
+        String str = "Shyam Kumar";
+        System.out.print(str);
+        System.out.println("Hello");
+    }
+}
